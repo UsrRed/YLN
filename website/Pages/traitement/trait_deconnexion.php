561ea7273e3f8338371aacededeb435be0d3a268
Nathan Martel <nathan.martel@etu.univ-tours.fr>
2023-09-26 15:12:37 +0200

    Add du fichier trait_deconnexion.php

---
+++ website/Pages/traitement/trait_deconnexion.php
@@ -0,0 +1,10 @@
+<?php
+session_start(); #Page uniquement accessible lorsque l'on est connecté
+
+# Détruit la session de l'utilisateur : 
+session_destroy();
+
+#Et ensuite on redirige vers la page de connexion
+
+header("Location: ../Connexion.php");
+exit();
