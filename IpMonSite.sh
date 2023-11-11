a75e202e783783bfc1607e0043997eb16e3a5326
22107454t <nathan.martel@etu.univ-tours.fr>
2023-11-11 13:42:55 -0500

    Correction des scripts de lancement #24

--- IpMonSite.sh
+++ IpMonSite.sh
@@ -9,26 +9,25 @@ if [ $# -ne 0 ]; then #Si le nombre n'arguments passés n'est pas de 0, message 
     exit 1
 fi
 
-#Normalement l'adresse IP du haproxy ne change, c'est 172.18.0.4, mais on s'assure quand meme
-
-AdresseIP=$(podman inspect haproxy | grep -oP '"IPAddress": "\K[^"]+') #Tout sauf un "" car il y a un attribut IpAddress="" par moment
-
 #AdresseIP=$(ip a | grep podman* | grep inet | grep -oP '(?<=inet )[\d.]+') #espace après inet pour ne prendre que l'adresse IP après inet, donc pas le brd et on regarde des chiffres ou des points (\d.)
 
-
-
-if podman ps | grep "nginx" && podman ps | grep "php" && podman ps | grep "mysql" && podman ps | grep "haproxy" #On regarde si les conteneurs sont lancés avec podman ps en greppant le nom des trois conteneurs
+if podman ps | grep "nginx1" && podman ps | grep "nginx2" && podman ps | grep "php" && podman ps | grep "mysql" && podman ps | grep "haproxy" #On regarde si les conteneurs sont lancés avec podman ps en greppant le nom des trois conteneurs
 then
     echo ""
-    echo "L'adresse IP de l'application sur laquelle se rendre est https://$AdresseIP:8443";
+    #echo "L'adresse IP de l'application sur laquelle se rendre est https://$AdresseIP:8443";
 else
-    echo "Le fichier docker-compose n'est pas ou ne s'est pas lancé"
+    echo "--> Le fichier docker-compose n'est pas ou ne s'est pas lancé"
     echo ""
-    echo "Lancement du fichier docker-compose ..."
+    echo "--> Lancement du fichier docker-compose ..."
     echo ""
-    podman-compose -f ./docker-compose.yaml up -d #Si la sortie podman ps n'inclut pas les trois conteneurs, on lance le .yaml
-    sleep 3
+    podman-compose -f ./docker-compose.yaml up -d #Si la sortie podman ps n'inclut pas les conteneurs, on lance le .yaml
     #AdresseIP=$(ip a | grep podman* | grep inet | grep -oP '(?<=inet )[\d.]+')
     echo ""
-    echo "L'adresse IP de l'application sur laquelle se rendre est https://$AdresseIP:8443";
+    #echo "L'adresse IP de l'application sur laquelle se rendre est https://$AdresseIP:8443";
 fi
+
+sleep 1
+
+AdresseIP=$(podman inspect haproxy | grep -oP '"IPAddress": "\K[^"]+') #Tout sauf un "" car il y a un attribut IpAddress="" par moment
+echo "--> L'adresse IP de l'application sur laquelle se rendre est https://$AdresseIP:8443";
+echo "--> Vous pouvez faire un CTRL + [clique gauche] sur l'URL ci-dessus.";
--- ScriptLancement.sh
+++ ScriptLancement.sh
@@ -2,43 +2,23 @@
 
 # Vérification si HAProxy est déjà installé
 
-if ! [ -x "$(command -v haproxy)" ]; then
-    echo "HaProxy n'est pas installé. Installation en cours..."
-    apt-get update
-    apt-get install -y haproxy
-else
-    echo "HaProxy est déjà installé."
-fi
-echo "#############################################"
-#Vérification si HAProxy est déjà lancé
-if systemctl is-active --quiet haproxy; then
-    echo "HAProxy est déjà en cours d'exécution"
-else
-    echo "Démarrage de HAProxy..."
-    systemctl start haproxy
-fi
 echo "############################################"
 #Vérification des images si elles sont bien présentes
-if podman images | grep "nginx" && podman images | grep "php" && podman ps | grep "mysql"; then
+if podman images | grep "docker.io/library/nginx" && podman images | grep "docker.io/library/php" && podman images | grep "docker.io/library/mysql" && podman images | grep "docker.io/library/haproxy"; then
     echo "Images des conteneurs présentes sur l'OS physique"
 else
     echo "Pulling des images pour le lancement des conteneurs.."
-    bash ./ScriptImage.sh
+    bash ScriptImage.sh
 fi
 echo "###########################################"
 # Vérification si les conteneurs sont déjà en cours d'exécution
-if podman ps | grep "nginx" && podman ps | grep "php" && podman ps | grep "mysql"; then
+if podman ps | grep "nginx" && podman ps | grep "php" && podman ps | grep "mysql" && podman ps | grep "haproxy"; then
     echo "Les conteneurs sont déjà en cours d'exécution."
 else
     echo "Démarrage des conteneurs..."
     podman-compose -f docker-compose.yaml up -d
 fi
 
-# Exécution du script haproxy_config.sh
-bash ./haproxy_config.sh
-
-# Démarrage de HAProxy avec votre configuration
-nohup haproxy -f loadbalancing/haproxy.cfg &
 echo "###########################################"
-# Exécution du script IpMonSite
-bash ./IpMonSite.sh
+# Pour voir l'adresse IP de l'application
+bash IpMonSite.sh
