70810ccd8c77356fd8f40dcf566a5f9e1b40ea1a
root <nathan.martel@etu.univ-tours.fr>
2023-11-15 15:26:19 +0100

    Style compteut blocage #32

--- website/Pages/traitement/trait_blocage.php
+++ website/Pages/traitement/trait_blocage.php
@@ -1,25 +1,44 @@
-<?php #include('/home/includes/header.php'); ?>
+<?#php include('/home/includes/header.php'); ?>
+<!DOCTYPE html>
 
-<body class="bg-light" disabled>
+<html>
+<head>
+	<title>SAE501-502-THEOTIME-MARTEL</title>
+	<meta charset="utf-8">
+	<link rel="icon" type="image/png" href="logo.jpg" />
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	<!-- Pour avoir bootstrap version 4.5.2 : https://getbootstrap.com/docs/4.5/getting-started/introduction/-->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+	<style>
+        	#countdown {
+			font-size: 300px;
+		}
+	</style>
+
+</head>
+
+<body class="bg-light" disabled> <!--Pour que l'utilisateur ne puisse rien faire -->
 <div class="container mt-5 text-center"> <!-- Ajout de la classe text-center pour centrer le contenu -->
-
-    <h2>Votre compte est temporairement bloqué</h2><br/><br/>
-    <p><span id="countdown" class="display-1">300</span></p> <!-- Ajout de la classe display-1 pour le texte en très gros -->
+    
+<br><br><br><h2>Votre compte est temporairement <b>bloqué</b>. Vous serez redirigé vers la page de connexion à la fin du compte a rebours</h2><br/><br/><br/><br/><br/><br/><br/>
+    <h1><span id="countdown" class="display-1">300</span></h1> <!-- Ajout de la classe display-1 pour le texte en très gros -->
 
     <script>
-        // Compteur à rebours en JavaScript
-        var seconds = 300; // 5 minutes
+       
+        var seconds = 301; 
         function countdown() {
             var minutes = Math.floor(seconds / 60);
             var remainingSeconds = seconds % 60;
 
-            // Ajoute un zéro devant les chiffres de moins de 10
+            
             remainingSeconds = remainingSeconds < 10 ? "0" + remainingSeconds : remainingSeconds;
 
             document.getElementById("countdown").textContent = minutes + ":" + remainingSeconds;
 
-            if (seconds <= 0) {
-                window.location.replace("/Connexion"); // On redirige après le compte à rebours
+	    if (seconds <= 0) {
+		window.location.replace("/Connexion");
             } else {
                 seconds--;
                 setTimeout(countdown, 1000);
