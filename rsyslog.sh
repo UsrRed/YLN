5bf276ec5c33793bd55bdb324759c68b821c7e31
22107454t <nathan.martel@etu.univ-tours.fr>
2023-11-24 16:53:16 -0500

    Stack ELS Elastic, Logtash, Syslog fonctionnelle mais beaucoup de ressource demandée

--- rsyslog.sh
+++ rsyslog.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 if [ $# -ne 0 ]; then
-    echo "Ce script ne prend pas de paramètre, il doit être exécuté sans paramètres"
-    exit 1
+	echo "Ce script ne prend pas de paramètre, il doit être exécuté sans paramètres"
+	exit 1
 fi
 
 #Pour installer rsyslog en fonction du SE de l'utilisateur
 
 if [ -x "$(command -v apt-get)" ]; then
-    sudo apt-get update 
-    sudo apt-get install -y rsyslog 
+	sudo apt-get update 
+	sudo apt-get install -y rsyslog 
 elif [ -x "$(command -v dnf)" ]; then
-    sudo dnf install -y rsyslog 
+	sudo dnf install -y rsyslog 
 elif [ -x "$(command -v yum)" ]; then
-    sudo yum install -y rsyslog 
+	sudo yum install -y rsyslog 
 else
-    echo "Système d'exploitation inconnu"
-    exit 1
+	echo "Système d'exploitation inconnu"
+	exit 1
 fi
 
 # Pour déplacer la configuration rsyslog : 
@@ -49,14 +49,14 @@ fi
 #On redémarre les services (journald et rsyslog) : 
 
 if [ -x "$(command -v systemctl)" ]; then
-    systemctl restart systemd-journald
-    systemctl restart rsyslog
-    echo "Services journald et rsyslog redémarrés avec succès"
+	systemctl restart systemd-journald
+	systemctl restart rsyslog
+	echo "Services journald et rsyslog redémarrés avec succès"
 
 elif [ -x "$(command -v service)" ]; then
-    service systemd-journald restart
-    service rsyslog restart
-    echo "Services journald et rsyslog redémarrés avec succès"
+	service systemd-journald restart
+	service rsyslog restart
+	echo "Services journald et rsyslog redémarrés avec succès"
 fi
 
 #Pull de l'image syslog-ng : 
